6852abde5c65e0300074888c52f9e179daef7148
Eren Orbey <erenorbey@microsoft.com>
2023-12-30 16:35:13 -0500

    Refactored for display and model output signature

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
  // Replace 'YOUR_BEARER_TOKEN' with the actual Bearer token value
- const bearerToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSIsImtpZCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3LyIsImlhdCI6MTcwMzk2MDExMCwibmJmIjoxNzAzOTYwMTEwLCJleHAiOjE3MDM5NjQ3OTQsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBWVFBZS84VkFBQUF1V25hcklYUDlZeGdHSldJVUVSdGo5YzJUODgrNURIUFRXR3JMcTlTVzhqUnpzSS91Rm4rTUtzY1lteE5ZalBOUkdseDFnVnlQT1dxaHdpMkRSQzVmWVlubXlvcVhzRnRyUnJVenRHbHZxbVFITWxRNDYwSnhXRzlzcUtXTnhXNXN1YVN4eVMvU0VRYTZRWlRVOWR6TU85SzI5REx2Q2xub3JxL0RGYi9PZjA9IiwiYW1yIjpbInJzYSIsIm1mYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImNvbnRyb2xzIjpbImFwcF9yZXMiXSwiY29udHJvbHNfYXVkcyI6WyIwMDAwMDAwOS0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiXSwiZGV2aWNlaWQiOiI1Yzk0OTE5Yy0wNTY2LTQyZDMtOGY0Ny05MmViY2UwNjYzNmYiLCJmYW1pbHlfbmFtZSI6Ik9yYmV5IiwiZ2l2ZW5fbmFtZSI6IkVyZW4iLCJpcGFkZHIiOiI3Ni4xMTkuMTgzLjExNCIsIm5hbWUiOiJFcmVuIE9yYmV5Iiwib2lkIjoiMDcwMTIyMmItZjNmZi00NTQ3LWJiOGItZWM0N2NhZTcwYzU3Iiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTEyNDUyNTA5NS03MDgyNTk2MzctMTU0MzExOTAyMS0yMDc1NzA0IiwicHVpZCI6IjEwMDMyMDAxOTJFNTI2NDIiLCJyaCI6IjAuQVJvQXY0ajVjdkdHcjBHUnF5MTgwQkhiUndrQUFBQUFBQUFBd0FBQUFBQUFBQUFhQVBjLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJkdmNfbW5nZCIsImR2Y19jbXAiLCJrbXNpIl0sInN1YiI6IkcyRnBiZWdTWkx3dEdCRmx5ZkE5cnBxd1cyb1ZETks3enNKc0JFTDlsRHciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1bmlxdWVfbmFtZSI6ImVyZW5vcmJleUBtaWNyb3NvZnQuY29tIiwidXBuIjoiZXJlbm9yYmV5QG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJnLVBBVl8zdXgwYXgtaGtPR3hrc0FRIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2NjIjpbIkNQMSJdfQ.olRjQCge42RTx6OTVriJYV67AZhGWcWRhofIjbkvkjsfd9463nEwJ4-T33tbORG4pFIwiG1opAjLH-xjiZig3pkrQeJJM-wI0JP9PuUACTI12PKQr_6euHMbatu3PErwGLMHSRXB2cl4YMRW_6nb9G-Bz1C28T83vg_fvgZ_mDHSmKqxb0adPy8eze2YVi4-mbc_9faRF-g3hC3x_TDibI_9QqTk4ouhK_gq2gFhBrwPYnXXLXKKiiS1WLrMMlzfjpGDVpRef5Ju7q8LNS42GJqB99MYfOZti6aUQ-cKZcSuYYvb4TvaLuUB5oSmW_IyhZYHRXQFdUSIi4uRvZo7fg'
- 
-// Event listener for endpoint URL input change
+const bearerToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSIsImtpZCI6IjVCM25SeHRRN2ppOGVORGMzRnkwNUtmOTdaRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3LyIsImlhdCI6MTcwMzk2OTEzOSwibmJmIjoxNzAzOTY5MTM5LCJleHAiOjE3MDM5NzQ3NjQsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBWVFBZS84VkFBQUFPQkE1QlJHZE1UUWVCZHRZcWViOEJYZFY1SmJNYnV2YThxd3ZEenJFMElTQ1p3VmluL3FCUDFhVzlETFZQU0lSejF2cW5HQndBSUYwWVBDN3E5Y2ZxblpMTW1SZS9jZCswVWhmeXlxSlpJeDNaM0JHVjZjOXphMkZNUCtSSDZqTi9pcGFKejdoNk5PbWRxM2JCVmd6bDNwK0RvaStwc2FQaUx3Rzcya1ZkRzA9IiwiYW1yIjpbInJzYSIsIm1mYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImNvbnRyb2xzIjpbImFwcF9yZXMiXSwiY29udHJvbHNfYXVkcyI6WyIwMDAwMDAwOS0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiXSwiZGV2aWNlaWQiOiI1Yzk0OTE5Yy0wNTY2LTQyZDMtOGY0Ny05MmViY2UwNjYzNmYiLCJmYW1pbHlfbmFtZSI6Ik9yYmV5IiwiZ2l2ZW5fbmFtZSI6IkVyZW4iLCJpcGFkZHIiOiI3Ni4xOS4xNjguMTk1IiwibmFtZSI6IkVyZW4gT3JiZXkiLCJvaWQiOiIwNzAxMjIyYi1mM2ZmLTQ1NDctYmI4Yi1lYzQ3Y2FlNzBjNTciLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMTI0NTI1MDk1LTcwODI1OTYzNy0xNTQzMTE5MDIxLTIwNzU3MDQiLCJwdWlkIjoiMTAwMzIwMDE5MkU1MjY0MiIsInJoIjoiMC5BUm9BdjRqNWN2R0dyMEdScXkxODBCSGJSd2tBQUFBQUFBQUF3QUFBQUFBQUFBQWFBUGMuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImR2Y19tbmdkIiwiZHZjX2NtcCIsImttc2kiXSwic3ViIjoiRzJGcGJlZ1NaTHd0R0JGbHlmQTlycHF3VzJvVkROSzd6c0pzQkVMOWxEdyIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInVuaXF1ZV9uYW1lIjoiZXJlbm9yYmV5QG1pY3Jvc29mdC5jb20iLCJ1cG4iOiJlcmVub3JiZXlAbWljcm9zb2Z0LmNvbSIsInV0aSI6IjBoLVVaV1lEOFUyZ3NUZlEyREE3QWciLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfY2MiOlsiQ1AxIl19.dgLQzep6D_gfENEyfvh4RzgnJnmi6Ek0kFxYmBh7puCweV05NAWmezGBaYonHZlE83OMwK74NeCFb2S6F9O9Ym6sFeg6rD0UYWcvfy_asq5FVxEbzK-zl3J6zStaLOl8poLLPvQa6Q4rQ83Aa2betcwBw6bIfbuYqZqSAvlVofYmUrlurqMxik9-S4GXZYvl7pfQlkiU_-9EkRVB5bSPaqflwtRPXKkUR6oyY0LFFIakxPFiNaQ1QAN2mZYLi7QK8jKr1Y5P7ru-X45SnwffuMrmiZ5s_f73ez4AX35K24oo0mb8mwu1MGwQTlUxwCvt4BX7HdL2dFXV36fDuh17KQ'
+
 document.getElementById('endpointUrl').addEventListener('input', function () {
-    // Clear existing input fields
+    // Clear existing input fields and output signature
     document.getElementById('keyValuePairs').innerHTML = '';
+    document.getElementById('output').innerHTML = '';
 
     const endpointUrl = document.getElementById('endpointUrl').value;
+    const submitButton = document.querySelector('#mlForm button[type="submit"]');
+    const modelOutput = document.getElementById('output');
+
+    // Hide model output
+    modelOutput.style.display = 'none';
 
     fetch(endpointUrl, {
         method: 'GET',
@@ -22,7 +27,6 @@ document.getElementById('endpointUrl').addEventListener('input', function () {
         return response.json();
     })
     .then(data => {
-        console.log(data)
         const endpointStatus = data.defaultStatus !== undefined ? data.defaultStatus : data.status;
         document.getElementById('endpointStatus').innerText = `Endpoint Status: ${endpointStatus}`;
 
@@ -32,40 +36,47 @@ document.getElementById('endpointUrl').addEventListener('input', function () {
         if (inputSignature) {
             createInputFields(JSON.parse(inputSignature));
         }
+
+        // Handle output signature and display it
+        const outputSignature = data.defaultVersionInfo?.outputSignature || data.outputSignature;
+
+        if (outputSignature) {
+            displayOutputSignature(JSON.parse(outputSignature));
+        }
+
+        // Show submit button if the endpoint status is "Running"
+        submitButton.style.display = endpointStatus === 'Running' ? 'block' : 'none';
     })
     .catch(error => {
         console.error('Error:', error);
         document.getElementById('endpointStatus').innerText = `Endpoint Status: Error`;
+
+        // Hide submit button if there's an error
+        submitButton.style.display = 'none';
     });
 });
+
 
 document.getElementById('mlForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const endpointUrl = document.getElementById('endpointUrl').value;
+    const modelOutput = document.getElementById('output');
+
+    // Hide model output
+    modelOutput.style.display = 'none';
 
     // Generate JSON payload based on individual values
     const keyValuePairs = document.querySelectorAll('#keyValuePairs .input-group');
-
     const individualData = {};
-    keyValuePairs.forEach(pair => {
-        // Check if it's a dynamically generated field
-        const isDynamicField = pair.querySelector('.input-group-prepend span') !== null;
 
-        if (isDynamicField) {
-            const label = pair.querySelector('.input-group-prepend span').textContent;
-            const value = pair.querySelector('.form-control').value;
-            individualData[label] = !isNaN(value) ? parseFloat(value) : value;
-        } else {
-            // It's a manually added field
-            const key = pair.querySelector('.form-control:nth-child(1)').value;
-            const value = pair.querySelector('.form-control:nth-child(2)').value;
-            individualData[key] = !isNaN(value) ? parseFloat(value) : value;
-        }
+    keyValuePairs.forEach(pair => {
+        const label = pair.querySelector('.input-group-prepend span').textContent;
+        const value = pair.querySelector('.form-control').value;
+        individualData[label] = !isNaN(value) ? parseFloat(value) : value;
     });
 
     const inputData = `[${JSON.stringify(individualData)}]`;
-    console.log(inputData)
 
     fetch(endpointUrl, {
         method: 'POST',
@@ -76,14 +87,15 @@ document.getElementById('mlForm').addEventListener('submit', function (e) {
         body: inputData,
     })
     .then(response => {
-        console.log(inputData)
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         return response.json();
     })
     .then(data => {
-        document.getElementById('output').innerText = `Model Output: ${data}`;
+        // Display model output
+        modelOutput.innerText = `Model Output: ${data}`;
+        modelOutput.style.display = 'block';
     })
     .catch(error => {
         console.error('Error:', error);
@@ -102,70 +114,60 @@ function createInputFields(signature) {
             <div class="input-group-prepend">
                 <span class="input-group-text">${field.name} (${field.type})</span>
             </div>
-            <input type="${inputType}" class="form-control" placeholder="${field.name}" data-type="${field.type}">
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary remove-key-value-btn" type="button">Remove</button>
-            </div>
+            <input type="${inputType}" step="any" class="form-control" placeholder="${field.name}" data-type="${field.type}">
         `;
         keyValuePairs.appendChild(newKeyValuePair);
 
-        // Add event listener to remove key/value pair
-        newKeyValuePair.querySelector('.remove-key-value-btn').addEventListener('click', function () {
-            newKeyValuePair.remove();
-        });
-
         // Add input event listener for validation
         const inputElement = newKeyValuePair.querySelector('.form-control');
-        inputElement.addEventListener('input', function () {
-            validateForm();
-        });
+        inputElement.addEventListener('input', validateForm);
     });
 
     // Initial validation
     validateForm();
-
-    // Function to validate the form
-    function validateForm() {
-        const inputs = document.querySelectorAll('#keyValuePairs input.form-control');
-        let formValid = true;
-
-        inputs.forEach(inputElement => {
-            const dataType = inputElement.getAttribute('data-type');
-
-            if (dataType === 'double' && isNaN(inputElement.valueAsNumber)) {
-                inputElement.style.border = '1px solid red';
-
-                // Add warning icon
-                inputElement.classList.add('is-invalid');
-                formValid = false;
-            } else {
-                inputElement.style.border = ''; // Remove the red border
-
-                // Remove warning icon
-                inputElement.classList.remove('is-invalid');
-            }
-        });
-
-        document.querySelector('button[type="submit"]').disabled = !formValid;
-    }
 }
 
-// Event listener for adding key/value pairs
-document.getElementById('addKeyValuePairBtn').addEventListener('click', function () {
-    const keyValuePairs = document.getElementById('keyValuePairs');
-    const newKeyValuePair = document.createElement('div');
-    newKeyValuePair.classList.add('input-group', 'mb-3');
-    newKeyValuePair.innerHTML = `
-        <input type="text" class="form-control" placeholder="Key">
-        <input type="text" class="form-control" placeholder="Value">
-        <div class="input-group-append">
-            <button class="btn btn-outline-secondary remove-key-value-btn" type="button">Remove</button>
-        </div>
-    `;
-    keyValuePairs.appendChild(newKeyValuePair);
+function displayOutputSignature(signature) {
+    const outputDiv = document.getElementById('output');
+    console.log(signature)
 
-    // Add event listener to remove key/value pair
-    newKeyValuePair.querySelector('.remove-key-value-btn').addEventListener('click', function () {
-        newKeyValuePair.remove();
+    signature.forEach(field => {
+        const outputField = document.createElement('div');
+        outputField.classList.add('output-field');
+        outputField.innerHTML = `
+            <p>${field.name} (${field.type})</p>
+        `;
+        outputDiv.appendChild(outputField);
     });
-});
+
+    // Display the output section
+    outputDiv.style.display = 'block';
+}
+
+// Function to validate the form
+function validateForm() {
+    const inputs = document.querySelectorAll('#keyValuePairs input.form-control');
+    let formValid = true;
+
+    inputs.forEach(inputElement => {
+        const dataType = inputElement.getAttribute('data-type');
+
+        if (dataType === 'double' && (isNaN(inputElement.valueAsNumber))) {
+            inputElement.style.border = '1px solid red';
+
+            // Add warning icon
+            inputElement.classList.add('is-invalid');
+            formValid = false;
+        } else {
+            inputElement.style.border = ''; // Remove the red border
+
+            // Remove warning icon
+            inputElement.classList.remove('is-invalid');
+        }
+    });
+
+    const submitButton = document.querySelector('#mlForm button[type="submit"]');
+    if (submitButton) {
+        submitButton.disabled = !formValid;
+    }
+}
